9f326ed1395a90702567104f81874f2d928c0732
pmarques93 <pmarques93@gmail.com>
2021-04-04 23:22:45 +0100

    Add initial enemy animations.

---
+++ Game/Assets/Scripts/Enemies/EnemyAnimations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Class responsible for handling player animations.
+/// </summary>
+public class EnemyAnimations : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    // Components
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("Speed", agent.remainingDistance);
+    }
+}
