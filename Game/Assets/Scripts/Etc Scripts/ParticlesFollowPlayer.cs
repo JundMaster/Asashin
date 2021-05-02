bb84c0d003e014e892e18eb0b517fdff11032567
pmarques93 <pmarques93@gmail.com>
2021-05-02 22:42:11 +0100

    Add rain/dust motes to follow player.

---
+++ Game/Assets/Scripts/Etc Scripts/ParticlesFollowPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class responsible for handling rain position.
+/// </summary>
+public class ParticlesFollowPlayer : MonoBehaviour, IFindPlayer
+{
+    private Player player;
+
+    public void FindPlayer()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    public void PlayerLost()
+    {
+        // Left blank on purpose
+    }
+
+    private void FixedUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 playerPos = 
+                new Vector3(
+                    player.transform.position.x, 
+                    transform.position.y, 
+                    player.transform.position.z);
+
+            transform.position = playerPos;
+        }
+    }
+}
