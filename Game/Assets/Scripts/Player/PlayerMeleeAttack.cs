29efc098503639aed81af6fa57a6bb66e3dd5ab7
pmarques93 <pmarques93@gmail.com>
2021-05-08 18:27:36 +0100

    Fix continue loading to area 1 bug.

--- Game/Assets/Scripts/Player/PlayerMeleeAttack.cs
+++ Game/Assets/Scripts/Player/PlayerMeleeAttack.cs
@@ -250,7 +250,7 @@ public class PlayerMeleeAttack : MonoBehaviour, IAction
             {
                 // Only happens if the player is BEHIND the enemy, prevents
                 // from doing instant kill while the enemy is behind the player
-                if (Vector3.Angle(dir, transform.forward) < 25)
+                if (Vector3.Angle(dir, transform.forward) < 50)
                 {
                     // Instant kill Anim.
                     OnLightMeleeAttack(false);
