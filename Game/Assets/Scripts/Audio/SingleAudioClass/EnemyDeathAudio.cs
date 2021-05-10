c547c3600b8ee709789379d394d1c4f494e56eb1
pmarques93 <pmarques93@gmail.com>
2021-05-10 16:51:56 +0100

    Add sound to enemy vanish smoke.

---
+++ Game/Assets/Scripts/Audio/SingleAudioClass/EnemyDeathAudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class responsible for playing enemy death audio.
+/// </summary>
+public class EnemyDeathAudio : AbstractSoundBase
+{
+    [SerializeField] private AbstractSoundScriptableObject enemyDeathAudio;
+
+    public override void PlaySound(Sound sound)
+    {
+        // Left blank on purpose
+    }
+
+    private void Start()
+    {
+        enemyDeathAudio.PlaySound(audioSource);
+    }
+}
