0691fbb0e7d35be102e796fce6625de8942f8dd9
pmarques93 <pmarques93@gmail.com>
2021-05-19 18:53:30 +0100

    Add tutorials text.

--- Game/Assets/Scripts/Tutorial/DisplayTutMessage.cs
+++ Game/Assets/Scripts/Tutorial/DisplayTutMessage.cs
@@ -20,7 +20,7 @@ public class DisplayTutMessage : MonoBehaviour
     private EventSystem eventSys;
     private PlayerInputCustom input;
 
-    private enum CurrentTutorial { _1, _2, _3, _4, _5, _6, _7, _8, _9, _10 }
+    private enum CurrentTutorial { _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13 }
     [SerializeField] private CurrentTutorial currentTutorial;
 
     private readonly string KEYBOARDMOVEMENT    = "WASD";
@@ -37,6 +37,8 @@ public class DisplayTutMessage : MonoBehaviour
     private readonly string GAMEPADLOOT         = "SQUARE";
     private readonly string KEYBOARDWALLHUG     = "MIDDLE MOUSE";
     private readonly string GAMEPADWALLHUG      = "CIRCLE";
+    private readonly string KEYBOARDWALLHUGMOVEMENT = "AD";
+    private readonly string GAMEPADWALLHUGMOVEMENT  = "LEFT ANALOG";
     private readonly string KEYBOARDITEMUSE     = "F";
     private readonly string GAMEPADITEMUSE      = "TRIANGLE";
     private readonly string KEYBOARDITEMLEFT    = "1";
@@ -59,6 +61,7 @@ public class DisplayTutMessage : MonoBehaviour
     private string block;
     private string loot;
     private string wallHug;
+    private string wallHugMovement;
     private string itemUse;
     private string itemLeft;
     private string itemRight;
@@ -173,6 +176,46 @@ public class DisplayTutMessage : MonoBehaviour
             case CurrentTutorial._3:
                 textMeshPro.text =
                     $"Notice that the enemy has a vision cone in front of him. This cone represents his visible area. Sneak through the bushes without beeing seen.";
+                break;
+            case CurrentTutorial._4:
+                textMeshPro.text =
+                    $"In Asashin, keep in mind that every action produces noise. In order to avoid producing noise you can sneak instead of running. Try to pass through the guards without alerting them.";
+                break;
+            case CurrentTutorial._5:
+                textMeshPro.text =
+                    $"You are able to attack or block attacks with your Ninjatō. To attack, choose a direction and press {attack}. To block, choose a direction and press {block}. Move towards the vision cone when you're ready.";
+                break;
+            case CurrentTutorial._6:
+                textMeshPro.text =
+                    $"If you approach an enemy off-guard you will be able to perform an instant kill. In order to do this, approach the enemy while sneaking without alerting him and press {attack}.";
+                break;
+            case CurrentTutorial._7:
+                textMeshPro.text =
+                    $"While exploring, you'll find wooden boxes and treasures with random quantities of loot inside. You can attack and break the wooden boxes and interact with treasures by pressing {loot}.";
+                break;
+            case CurrentTutorial._8:
+                textMeshPro.text =
+                    $"If you approach a wall, you can wall hug in order to be more stealthy and approach an enemy with care. In order to wall hug, approach a wall and press {wallHug}. You can move by pressing {wallHugMovement}.";
+                break;
+            case CurrentTutorial._9:
+                textMeshPro.text =
+                    $"In the bottom right corner you'll find the current items you have. In order to choose an item press {itemLeft} or {itemRight} and use that item with {itemUse}.";
+                break;
+            case CurrentTutorial._10:
+                textMeshPro.text =
+                    $"In order to have better control over your enemy, you are able to target him. While targeting the enemy, you are able to attack, block and use items in his direction. To target an enemy press {target}. To switch to next target press {targetLeft} or {targetRight}.";
+                break;
+            case CurrentTutorial._11:
+                textMeshPro.text =
+                    $"In order to avoid attacks you can roll by pressing {roll}.";
+                break;
+            case CurrentTutorial._12:
+                textMeshPro.text =
+                    $"If you roll in the exact moment an enemy is attacking, you'll perform a dodge. This dodge action will perform a slow motion over some seconds, giving you a big advantage in fights against various enemies. Move towards the vision cone when you're ready.";
+                break;
+            case CurrentTutorial._13:
+                textMeshPro.text =
+                    $"In this last tutorial you'll fight an enemy using the techniques you have learned so far. Move towards the vision cone when you're ready.";
                 break;
         }
     }
